1bdc53bb6d07fec4bfd758c42fec3106ed718dd1
Duartess <dandandsoares@gmail.com>
2025-03-22 00:39:48 -0300

    new menu

---
+++ assets/js/script.js
@@ -0,0 +1,2 @@
+const menuItems = document.querySelectorAll('.menu-item'); // Seleciona todos os itens de menu
+const closeMenuCheckbox = document.getElementById('close-menu'); // Seleciona o checkbox do menu
